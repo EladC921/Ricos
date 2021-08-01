8f1db0d5fff708035f41d7306361b0114a1f0a91
Bar Karta <69091505+BarKarta@users.noreply.github.com>
2021-08-01 15:02:49 +0300

    Changes - god please help us :)

---
+++ Ricos-App/src/Components/Header.js
@@ -0,0 +1,8 @@
+const Header = () =>{
+    return(
+        <div>
+        <img src="..//img//Logo.png"/>
+        </div>
+    )
+}
+export default Header;
---
+++ Ricos-App/src/Components/Main.js
@@ -0,0 +1,6 @@
+const Main = () =>{
+    return(
+        <h1>This is the Main!</h1>
+    )
+}
+export default Main;
