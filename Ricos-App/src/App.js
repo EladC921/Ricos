c620014b3125a930390407586fdb09199af96426
Shai Raz <shayy9999@gmail.com>
2021-08-03 15:13:12 +0300

    base routing added

--- Ricos-App/src/App.js
+++ Ricos-App/src/App.js
@@ -1,14 +1,19 @@
+import { Route, Switch } from "react-router-dom";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import "./css/app.css"
 
 const App = () => {
-    return (
-        <div className="container">
-            <Header />
-            <Main />
-        </div>
-    );
+  return (
+    <div className="container">
+      <Switch>
+        {/*<Route exact path="/login" component={Login} />*/}
+        <Route component={Header} />
+      </Switch>
+      <Route exact path="/" component={Main} />
+
+    </div>
+  );
 }
 
 export default App;
